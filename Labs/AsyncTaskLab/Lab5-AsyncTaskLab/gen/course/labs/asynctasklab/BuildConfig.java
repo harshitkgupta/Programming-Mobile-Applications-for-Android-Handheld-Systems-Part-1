108a6a7dfb48a3717d64c4edbca8357e162826b4
harshit-knit <harshit.knit@gmail.com>
2015-05-02 14:19:30 +0530

    first commit

---
+++ Labs/AsyncTaskLab/Lab5-AsyncTaskLab/gen/course/labs/asynctasklab/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package course.labs.asynctasklab;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Labs/Modern Art UI/SourceFiles/gen/com/brus/modernartui/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.brus.modernartui;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
